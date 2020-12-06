918b4e40d74415b58765674de6bbfd8bdef276a8
木本栞 <kimotoshiori@kimotonobuyukinoMacBook-Air.local>
2020-12-06 18:01:49 +0900

    kadai4made

--- src/Item2.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Item2 = () => {
-    return <h1>Item2</h1>
-}
-
-export default Item2
--- src/List.js
+++ src/List.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import Item from './Item'
-import Item2 from './Item2'
 
 const List = () => {
     return (
         <>
             <h1>List</h1>
             <Item />
-            <Item2 />
+            <Item />
         </>
         
     )
